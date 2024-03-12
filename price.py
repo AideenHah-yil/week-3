4e68f9aa0cd0c56b95657c00b4725f81dd84dd04
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-12 05:38:51 -0700

    initial commit

---
+++ price.py
@@ -0,0 +1,8 @@
+def calculate_discount(price, discount_percent):
+    final_price = price * discount_percent
+    result = final_price 
+
+    if discount_percent >= 20% :
+    return final_price
+    else:
+    return price
