e1bdb4e68444671f2e330e6b84863446d32d71cb
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-12 05:42:53 -0700

    correcting code

--- price.py
+++ price.py
@@ -1,8 +1,6 @@
 def calculate_discount(price, discount_percent):
-    final_price = price * discount_percent
-    result = final_price 
-
-    if discount_percent >= 20% :
-    return final_price
+    if discount_percent >= 20:
+        final_price = price - (price * discount_percent / 100)
+        return final_price
     else:
-    return price
+        return price
