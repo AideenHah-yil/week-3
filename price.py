02405fc2a324cbc47bf70d67c26ce60de6a30667
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-12 05:53:51 -0700

    solving task

--- price.py
+++ price.py
@@ -4,3 +4,18 @@ def calculate_discount(price, discount_percent):
         return final_price
     else:
         return price
+    
+
+def main():
+    original_price = float(input("Enter the original price of item: "))
+    discount_percent = float(input("Enter the discount percentage: "))
+
+    #calculate final price using calcutate_discount function
+    final_price = calculate_discount(original_price, discount_percent)
+
+    #print final price
+    if final_price != original_price:
+        print(f"The final price is: ${final_price:.2f}")
+
+    else:
+        print(f"The final price is: ${original_price:.2f}")
